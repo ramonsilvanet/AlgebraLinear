0f819a670a5935a18a9db8fe9a54e15e33f4f80c
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2016-10-03 12:12:56 -0300

    Eliminação Gaussiana

--- src/net/ramonsilva/Main.java
+++ src/net/ramonsilva/Main.java
@@ -3,6 +3,16 @@ package net.ramonsilva;
 public class Main {
 
     public static void main(String[] args) {
+        double[][] d = { { 1, 2, 3 }, { 4, 5, 6 }, { 9, 1, 3} };
+        Matriz D = new Matriz(d);
+        D.imprimir();
+        System.out.println();
 
+        Matriz A = Matriz.gerarMatrixAleatoria(5, 5);
+        Matriz b = A.transpose();
+
+        Matriz x = A.resolverPorEliminacaoGaussiana(b);
+        x.imprimir();
+        System.out.println();
     }
 }
--- src/net/ramonsilva/util.java
+++
@@ -1,7 +0,0 @@
-package net.ramonsilva;
-
-/**
- * Created by ramonsilva on 03/10/16.
- */
-public class util {
-}
