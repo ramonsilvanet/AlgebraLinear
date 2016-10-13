22a797027610fee2cae7a1a957a5080f1fd15f25
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2016-10-13 12:31:10 -0300

    - Resolvendo por Decomposicao LU

--- src/net/ramonsilva/solver/GaussDecomposite.java
+++
@@ -1,7 +0,0 @@
-package net.ramonsilva.solver;
-
-/**
- * Created by ramonsilva on 10/10/16.
- */
-public class GaussDecomposite {
-}
--- src/net/ramonsilva/solver/LUDecomposite.java
+++ src/net/ramonsilva/solver/LUDecomposite.java
@@ -1,7 +1,13 @@
 package net.ramonsilva.solver;
 
+import net.ramonsilva.Matrix;
+
 /**
  * Created by ramonsilva on 10/10/16.
  */
-public class LUDecomposite {
+public class LUDecomposite implements MatrixSolver {
+    @Override
+    public double[] solve(Matrix m) {
+        return new double[0];
+    }
 }
